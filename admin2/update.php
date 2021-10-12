e4ca50006608f1cf3ea0c233ef59e0edd7b09ed6
bandito228 <92264227+bandito228@users.noreply.github.com>
2021-10-12 20:17:42 +0300

    Update update.php

--- admin2/update.php
+++ admin2/update.php
@@ -6,6 +6,7 @@ $artcls_id = $_GET['id'];
 
 $artcls = mysqli_query($connection,"SELECT * FROM `artcl` WHERE `id`='$artcls_id'");
 $artcls = mysqli_fetch_assoc($artcls);
+
 ?>
 
 
@@ -22,11 +23,11 @@ $artcls = mysqli_fetch_assoc($artcls);
     <form action="update2.php" method="post">
         <p>Заголовок</p>
         <input type="hidden" name="id" value="<?=$artcls['id']?>" >
-        <input type="text" name="title" value="<?=$title['title']?>">
+        <input type="text" name="title" value="<?=$artcls['title']?>">
         <p>Фото</p>
-        <input type="text" name="img" value="<?=$img['img']?>">
+        <input type="text" name="img" value="<?=$artcls['img']?>">
         <p>Текст</p>
-        <textarea name="text"><?= $text['text']?></textarea>
+        <textarea name="text"><?= $artcls['text']?></textarea>
 
 
          <br><br>
