fdf08cdeaf28c298bfab8587f5cee6ca12ac32ae
bandito228 <92264227+bandito228@users.noreply.github.com>
2021-10-12 18:40:31 +0300

    delete

---
+++ admin2/delete.php
@@ -0,0 +1,11 @@
+<?php
+include "../includes/config.php";
+
+
+$id=$_GET['id'];
+
+mysqli_query($connection,"DELETE FROM `artcl` WHERE `artcl`.`id` = '$id'");
+
+
+header("Location: http://test/admin2/main.php");
+
